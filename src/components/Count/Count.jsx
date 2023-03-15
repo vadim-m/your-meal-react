61242dc306940017c1a3ed930c54196b042ed4ca
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-03-15 17:02:03 +0300

    feat: add JS functional on +/- cart buttons

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import style from "./Count.module.css";
 
 export const Count = (props) => {
+  const [count, setCount] = useState(props.count);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div className={style.count}>
-      <button className={style.minus}>-</button>
-      <p className={style.amount}>{props.count}</p>
-      <button className={style.plus}>+</button>
+      <button
+        className={style.minus}
+        onClick={decreaseCount}
+        disabled={count === 1}
+      >
+        -
+      </button>
+      <p className={style.amount}>{count}</p>
+      <button className={style.plus} onClick={increaseCount}>
+        +
+      </button>
     </div>
   );
 };
